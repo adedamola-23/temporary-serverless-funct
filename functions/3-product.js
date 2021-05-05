0e338201bb787b8c66bd336bdb66a30cfc219aa7
adedamola-23 <ahighprogrammer@gmail.com>
2021-05-05 13:14:54 -0700

    example 3 complete

---
+++ functions/3-product.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const Airtable = require('airtable-node');
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY})
+  .base('appO4kMBc3Q2o9hNQ')
+  .table('products')
+
+exports.handler = async (event,context) =>{
+  const {id} = event.queryStringParameters
+  if (id) {
+    try {
+      const product = await airtable.retrieve(id)
+      if (product.error) {
+               return{
+   statusCode: 404,
+   body: `no product with id: ${id}`,
+ }
+      }
+      return{
+   statusCode: 200,
+   body: JSON.stringify(product),
+ }
+    } catch (error) {
+      return {
+        statusCode:500,
+        body: `Server Error`
+      }
+    }
+    
+  }
+ return{
+   statusCode: 400,
+   body: 'pleasee provide id',
+ }
+}
